0660cf782a29f202802fc90c59b1cc387125e3e1
rlittletht <rlittle@thetasoft.com>
2016-10-16 00:07:56 -0700

    Checkpoint.
    Sounds are working (UpcAlert.*), scanner is working (hosted in a control embedded in the xaml page)
    (scanner.*).

--- UniversalUpc/MainPage.xaml.cs
+++ UniversalUpc/MainPage.xaml.cs
@@ -1,17 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Media.Capture;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
+using ZXing;
+using ZXing.Common;
+using ZXing.Mobile;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -22,9 +30,39 @@ namespace UniversalUpc
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private Scanner m_ups;
+        private UpcAlert m_upca;
+
         public MainPage()
         {
             this.InitializeComponent();
+
+            m_ups = new Scanner(this.Dispatcher, scannerControl);
+            m_upca = new UpcAlert();
+        }
+
+        private void DisplayResult(Result result)
+        {
+            if (result != null)
+                {
+                txtStatus.Text = "result != null, format = " + result.BarcodeFormat + ", text = " + result.Text;
+                m_upca.Play(UpcAlert.AlertType.GoodInfo);
+                }
+            else
+                {
+                txtStatus.Text = "result = null";
+                }
+        }
+
+        private async void ToggleScan(object sender, RoutedEventArgs e)
+        {
+            m_ups.SetupScanner(null, true);
+            m_ups.StartScanner(DisplayResult);
+        }
+
+        private void txtStatus_ContextCanceled(UIElement sender, RoutedEventArgs args)
+        {
+
         }
     }
 }
---
+++ UniversalUpc/UpcAlert.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+using System.Reflection;
+using System.IO;
+using Windows.Storage;
+using Windows.UI.Core;
+
+namespace UniversalUpc
+{
+    public class UpcAlert
+    {
+        public enum AlertType
+            {
+            GoodInfo,
+            BadInfo,
+            Halt,
+            Duplicate,
+			Drink
+            };
+
+        private Dictionary<AlertType, MediaElement> m_mpAlertMedia;
+
+        private async Task<MediaElement> LoadSoundFile(string v)
+        {
+            MediaElement snd = new MediaElement();
+
+            snd.AutoPlay = false;
+            StorageFolder folder = await Windows.ApplicationModel.Package.Current.InstalledLocation.GetFolderAsync("Assets");
+            StorageFile file = await folder.GetFileAsync(v);
+            var stream = await file.OpenAsync(FileAccessMode.Read);
+            snd.SetSource(stream, file.ContentType);
+            return snd;
+        }
+        async void LoadEffects()
+        {
+            m_mpAlertMedia.Add(AlertType.GoodInfo, await LoadSoundFile("Exclamation.wav"));
+            m_mpAlertMedia.Add(AlertType.Duplicate, await LoadSoundFile("doh.wav"));
+            m_mpAlertMedia.Add(AlertType.Drink, await LoadSoundFile("hicup_392.wav"));
+            m_mpAlertMedia.Add(AlertType.BadInfo, await LoadSoundFile("Ding.wav"));
+            m_mpAlertMedia.Add(AlertType.Halt, await LoadSoundFile("Ding.wav"));
+        }
+        public UpcAlert()
+        {
+            m_mpAlertMedia = new Dictionary<AlertType, MediaElement>();
+
+            LoadEffects();
+        }
+
+        public async void Play(AlertType at)
+        {
+            MediaElement me = m_mpAlertMedia[at];
+
+            await me.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+                {
+                me.Stop();
+                me.Play();
+                });
+        }
+#if none
+        void DoAlert(AlertType at, string s)
+		{
+		    // SystemSound ssnd;
+		    
+            // string sSound;
+            switch (at)
+                {
+                case AlertType.GoodInfo:
+                    SystemSounds.Exclamation.Play();
+                    // sSound = "SystemExclamation";
+                    break;
+                case AlertType.Duplicate:
+                    m_snd.Stream = Resource1.doh; // m_rm.GetStream("doh.wav");
+                    m_snd.PlaySync();
+                    break;
+				case AlertType.Drink:
+					m_snd.Stream = Resource1.hicup_392; // m_rm.GetStream("doh.wav");
+					m_snd.PlaySync();
+					break;
+                default:
+                    SystemSounds.Hand.Play();
+                    // sSound = "SystemHand";
+                    break;
+                }
+
+           
+       //      Win32.PlaySound(sSound, 0, (int)(Win32.SND.SND_ASYNC | Win32.SND.SND_ALIAS | Win32.SND.SND_NOWAIT));
+            UpdateStatus(s);
+        }
+#endif
+    }
+}
