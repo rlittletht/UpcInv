3e0b5a31a0d4688440a27a6c3bdeb4af6bfe9e6b
Rob Little <rlittletht@users.noreply.github.com>
2020-07-12 23:05:15 -0700

    Add heartbeat API to service

---
+++ UpcApi/Controllers/diagnostics.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using TCore.Logging;
+
+namespace UpcApi.Controllers
+{
+    public class DiagnosticController : ApiController
+    {
+        enum DiagnosticResult
+        {
+            ServiceRunning
+        }
+
+        [HttpGet]
+        [Route("api/diagnostics/Heartbeat")]
+        public IHttpActionResult Heartbeat()
+        {
+            return Ok(DiagnosticResult.ServiceRunning);
+        }
+    }
+}
+
